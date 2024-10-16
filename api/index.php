5fba23990523754e5f798cc33d1bf2379a6a72e1
VeraMatiasE <89822359+VeraMatiasE@users.noreply.github.com>
2024-10-16 18:06:29 -0300

    Fixed index.php

--- api/index.php
+++ api/index.php
@@ -199,11 +199,11 @@
   // 16 - Lectura de un archivo de texto.
   echo "<h1>16 - Lectura de un archivo de texto.</h1>";
   if (
-    $ar_pedido = fopen("./pages/pedido.txt", "a")
+    $ar_pedido = fopen("./pages/pedido.txt", "r")
   ) {
     while (!feof($ar_pedido)) {
       $linea = fgets($ar_pedido);
-      $lineasalto = nl2br($ar_pedido);
+      $lineasalto = nl2br($linea);
       echo $lineasalto;
     }
     fclose($ar_pedido);
