03eda6b3e790a138b5f5708c632be85f5cb8456e
VeraMatiasE <89822359+VeraMatiasE@users.noreply.github.com>
2024-10-22 22:13:21 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -10,7 +10,7 @@ conectar();
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Actividad Aulica 18</title>
-  <link rel="stylesheet" href="styles/style.css" />
+  <link rel="stylesheet" href="/styles/style.css" />
 </head>
 
 <body>
